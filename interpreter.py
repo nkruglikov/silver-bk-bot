e8a39763f78236fe8d7aca1eb20f834a38f42bda
Nikolai Kruglikov <kkrugl@yandex.ru>
2015-07-26 23:36:21 +0500

    Add commands 'routes' and 'commands'

--- interpreter.py
+++ interpreter.py
@@ -24,6 +24,11 @@ def print_routes(routes):
     for no, route in enumerate(routes, 1):
         print('  {0:2}. {1}'.format(no, route[:-4]))
 
+def print_commands():
+    print(messages['available_commands'])
+    for command in sorted(set(commands) | set(system_commands.values())):
+        print(command)
+
 def run(fh, bot):
     if fh == sys.stdin:
         print('> ', end='')
@@ -37,36 +42,45 @@ def run(fh, bot):
             except ValueError:
                 pass
 
-        if command in commands:
-            print(command)
-            try:
-                exec('bot.{0}(*arguments)'.format(commands[command]))
-            except bkbot.BotParsingError as err:
-                print(messages['parsing_error'])
-            except bkbot.BotUsageError:
-                print(messages['usage_error'])
-            except Exception as err:
-                print(messages['error'])
-                print(err)
-        elif command.startswith('%'):
-            try:
-                route = command[1:]
-                route_fh = open('routes\\' + route + '.txt')
-                print(messages['route'] + ': ' + route)
-                run(route_fh, bot)
-            except FileNotFoundError:
-                print(messages['route_name_error'])
-        elif command == system_commands['debug']:
-            route_fh = sys.stdin
-            run(route_fh, bot)
-        elif command == system_commands['exit']:
-            return
-        else:
-            print(messages['command_error'])
-
+        execute_command(command)
+        
         if fh == sys.stdin:
             print('> ', end='')
             sys.stdout.flush()
+
+def execute_command(command):
+    if command in commands:
+        print(command)
+        try:
+            exec('bot.{0}(*arguments)'.format(commands[command]))
+        except bkbot.BotParsingError as err:
+            print(messages['parsing_error'])
+        except bkbot.BotUsageError:
+            print(messages['usage_error'])
+        except Exception as err:
+            print(messages['error'])
+            print(err)
+    elif command.startswith('%'):
+        try:
+            route = command[1:]
+            route_fh = open('routes\\' + route + '.txt')
+            print(messages['route'] + ': ' + route)
+            run(route_fh, bot)
+        except FileNotFoundError:
+            print(messages['route_name_error'])
+    elif command == system_commands['debug']:
+        route_fh = sys.stdin
+        run(route_fh, bot)
+    elif command == system_commands['exit']:
+        return
+    elif command == system_commands['commands']:
+        print_commands()
+    elif command == system_commands['routes']:
+        routes = detect_routes('routes')
+        print_routes(routes)
+    else:
+        print(messages['command_error'])
+
 
 def main():
     routes = detect_routes('routes')
